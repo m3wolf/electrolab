e13cee2240fd359dc246ecd3f45b889d0ecf61be
Mark Wolf <mwolf22@uic.edu>
2015-02-23 14:16:08 -0600

    Initial commit: generates slam files (w/o integration) and plots resulting map

---
+++ samples.py
@@ -0,0 +1,294 @@
+# -*- coding: utf-8 -*-
+
+import jinja2, math
+from matplotlib import pylab, pyplot, collections, patches, colors
+import numpy as np
+import pandas as pd
+
+class Experiment():
+    """Electrochemical experiment cycling on one channel."""
+    cycles = []
+
+    def __init__(self, df, mass=None, *args, **kwargs):
+        self._df = df
+        all_cycles = self._df.loc[self._df['mode']!=3]
+        # Calculate capacity from charge
+        if mass:
+            # all_cycles['capacity'] = all_cycles.loc('(Q-Qo)/mA.h')/mass
+            all_cycles.loc[:,'capacity'] = all_cycles.loc[:,'(Q-Qo)/mA.h']/mass
+        # Split the data into cycles, except the initial resting phase
+        self.cycles = list(all_cycles.groupby('cycle number'))
+        super(Experiment, self).__init__(*args, **kwargs)
+
+    def plot_cycles(self, xcolumn, ycolumn):
+        """Plot each electrochemical cycle"""
+        pylab.xlabel(xcolumn)
+        pylab.ylabel(ycolumn)
+        for cycle in self.cycles:
+            pylab.plot(cycle[1][xcolumn], cycle[1][ycolumn])
+
+    def plot_cycles_derivative(self, xcolumn, ycolumn):
+        """Plot the first derivative of each of the cycles"""
+        pylab.xlabel(xcolumn)
+        pylab.ylabel(ycolumn)
+        for cycle in self.cycles:
+            yder = np.gradient(cycle[1].loc[:, ycolumn])
+            pylab.plot(cycle[1][xcolumn], yder)
+
+class Cycle():
+    """Data from one charge-discharge cycle."""
+    pass
+
+
+class Cube():
+    """Cubic coordinates of a hexagon"""
+    def __init__(self, i, j, k, *args, **kwargs):
+        self.i = i
+        self.j = j
+        self.k = k
+        super(Cube, self).__init__(*args, **kwargs)
+
+    def __getitem__(self, key):
+        coord_list = [self.i, self.j, self.k]
+        return coord_list[key]
+
+    def __add__(self, other):
+        new = Cube(
+            self.i + other.i,
+            self.j + other.j,
+            self.k + other.k,
+        )
+        return new
+
+
+    def __eq__(self, other):
+        result = False
+        if self.i == other.i and self.j == other.j and self.k == other.k:
+            result = True
+        return result
+
+    def __str__(self):
+        return "({i}, {j}, {k})".format(i=self.i, j=self.j, k=self.k)
+
+    def __repr__(self):
+        return "Cube{0}".format(self.__str__())
+
+
+class BaseSample():
+    """
+    A physical sample that gets mapped by XRD, presumed to be circular
+    with center and diameter in millimeters.
+    """
+    cmap_name = 'autumn'
+    two_theta_range = (50, 90) # Detector angle range in degrees
+    THETA1_MIN=0 # Source limits based on geometry
+    THETA1_MAX=50
+    THETA2_MIN=0 # Detector limits based on geometry
+    THETA2_MAX=55
+    scan_time = 15 # Seconds at each detector angle
+    frame_step = 15 # How much to move detector by in degrees
+    frame_width = 20 # 2-theta coverage of detector face
+    scans = []
+    def __init__(self, center, diameter, rows,
+                 sample_name='unknown', *args, **kwargs):
+        self.center = center
+        self.diameter = diameter
+        self.rows = rows
+        self.sample_name = sample_name
+        return super(BaseSample, self).__init__(*args, **kwargs)
+
+    @property
+    def unit_size(self):
+        return self.diameter / self.rows / math.sqrt(3)
+
+    def create_scans(self):
+        """Populate the scans array with new scans in a hexagonal array."""
+        self.scans = []
+        for idx, coords in enumerate(self.path(self.rows)):
+            filename = '{sample}-{n:x}'.format(
+                sample=self.sample_name,
+                n=idx
+            )
+            new_scan = Scan(coords, filename)
+            self.scans.append(new_scan)
+
+    def path(self, rows):
+        """Generator gives coordinates for a spiral path around the sample."""
+        # Six different directions one can move
+        basis_set = {
+            'W': Cube(-1, 1, 0),
+            'SW': Cube(-1, 0, 1),
+            'SE': Cube(0, -1, 1),
+            'E': Cube(1, -1, 0),
+            'NE': Cube(1, 0, -1),
+            'NW': Cube(0, 1, -1)
+        }
+        # Start in the center
+        curr_coords = Cube(0, 0, 0)
+        yield curr_coords
+        # Spiral through each row
+        for row in range(1, rows+1):
+            # Move to next row
+            curr_coords += basis_set['NE']
+            yield curr_coords
+            for i in range(0, row-1):
+                curr_coords += basis_set['NW']
+                yield curr_coords
+            # Go around the ring for each basis vector
+            for key in ['W', 'SW', 'SE', 'E', 'NE']:
+                vector = basis_set[key]
+                for i in range(0, row):
+                    curr_coords += vector
+                    yield curr_coords
+
+    def get_metric(self, scan):
+        """
+        Accepts a scan-like object and returns the calculated
+        metric. Intended to be overwritten by subclasses for specific
+        sample materials.
+        """
+        # Just return the distance from bottom left to top right
+        r = (scan.cube_coords[0] + self.rows)/2
+        return r
+
+    def to_slam(self):
+        """Format the sample into a slam file that GADDS can process."""
+        # Import template
+        env = jinja2.Environment(loader=jinja2.PackageLoader('electrolab', ''))
+        template = env.get_template('mapping-template.slm')
+        self.create_scans()
+        context = self.get_context()
+        return template.render(**context)
+
+    def get_context(self):
+        """Convert the object to a dictionary for the templating engine."""
+        # Estimate the total time
+        secs = len(self.scans)*self.scan_time
+        context = {
+            'scans': [],
+            'num_scans': len(self.scans),
+            'number_of_frames': self.get_number_of_frames(),
+            'theta1': self.get_theta1(),
+            'theta2': self.get_theta2_start(),
+            'scan_time': self.scan_time,
+            'total_time': "{0}s ({1:0.1f}h)".format(secs, secs/3600),
+            'sample_name': self.sample_name
+        }
+        for idx, scan in enumerate(self.scans):
+            # Prepare scan-specific details
+            x, y = scan.xy_coords(unit_size=self.unit_size)
+            d = {'x': x, 'y': y, 'filename': scan.filename}
+            context['scans'].append(d)
+        return context
+
+    def get_number_of_frames(self):
+        theta1 = self.get_theta1()
+        num_frames = math.ceil(
+            (self.two_theta_range[1]-self.two_theta_range[0])/self.frame_step
+        )
+        # Check for values outside instrument limits
+        t2_start = self.get_theta2_start()
+        t2_end = t2_start + num_frames*self.frame_step
+        if t2_end > self.THETA2_MAX:
+            msg = "2-theta range {given} is outside detector limits: {limits}".format(
+                given=self.two_theta_range,
+                limits=(self.THETA2_MIN, self.THETA2_MAX))
+            raise ValueError(msg)
+        return num_frames
+
+    def get_theta2_start(self):
+        # Assuming that theta1 starts at highest possible range
+        theta1 = self.get_theta1()
+        theta2_bottom = self.two_theta_range[0] - theta1
+        theta2_start = theta2_bottom + self.frame_step/2
+        return theta2_start
+
+    def get_theta1(self):
+        # Check for values outside preset limits
+        theta1 = self.two_theta_range[0]
+        if theta1 < self.THETA1_MIN:
+            msg = "2-theta range {given} is outside source limits: {limits}".format(
+                given=self.two_theta_range,
+                limits=(self.THETA1_MIN, self.THETA1_MAX))
+            raise ValueError(msg)
+        elif theta1 > self.THETA1_MAX:
+            theta1 = self.THETA1_MAX
+        return theta1
+
+    def get_cmap(self):
+        """Return a function that converts values in range 0 to 1 to colors."""
+        return pyplot.get_cmap(self.cmap_name)
+
+    def plot_map(self, ax=None):
+        x = []
+        y = []
+        values = []
+        i = 0
+        cmap = self.get_cmap()
+        for scan in self.scans:
+            i+=1
+            coord = scan.xy_coords(self.unit_size)
+            x.append(coord[0])
+            y.append(coord[1])
+            values.append(self.get_metric(scan)/self.rows)
+        xy = list(zip(x, y))
+        # Convert values to colors
+        colors = [cmap(val) for val in values]
+        # Build and show the hexagons
+        if not ax:
+            # New axes unless one was already created
+            fig = pyplot.figure(figsize=(5, 5))
+            ax = pyplot.gca()
+        xy_lim = self.diameter/2*1.25
+        ax.set_xlim([-xy_lim, xy_lim])
+        ax.set_ylim([-xy_lim, xy_lim])
+        ax.set_xlabel('mm')
+        ax.set_ylabel('mm')
+        for key, loc in enumerate(xy):
+            hexagon = patches.RegularPolygon(xy=loc,
+                                             numVertices=6,
+                                             radius=0.57*self.unit_size,
+                                             color=colors[key])
+            ax.add_patch(hexagon)
+        # Add circle for theoretical edge
+        circle = patches.Circle((0, 0), radius=self.diameter/2,
+                                edgecolor='blue', fill=False, linestyle='dashed')
+        ax.add_patch(circle)
+        return ax
+
+class Scan():
+    """
+    An XRD scan at one X,Y location. Several Scan objects make up a
+    Sample object.
+    """
+    def __init__(self, location, filename, *args, **kwargs):
+        self.cube_coords = location
+        self.filename = filename
+        return super(Scan, self).__init__(*args, **kwargs)
+
+    def xy_coords(self, unit_size=1):
+        # Convert internal coordinates to conventional cartesian coords
+        cube = self.cube_coords
+        x = unit_size * 1/2 * (cube.i - cube.j)
+        y = unit_size * math.sqrt(3)/2 * (cube.i + cube.j)
+        return (x, y)
+
+    def load_spectrum(self):
+        filename = "{0}.plt".format(self.filename)
+        df = pd.read_csv(filename, names=['2theta', 'counts'],
+                         sep=' ', comment='!')
+        return df
+
+    def plot_spectrum(self, ax=None):
+        df = self.load_spectrum()
+        if not ax:
+            fig = pyplot.figure()
+            ax = pyplot.gca()
+        ax.plot(df.loc[:, '2theta'], df.loc[:, 'counts'])
+        ax.set_xlabel('2θ')
+        ax.set_ylabel('Counts')
+        title = 'XRD Spectrum at ({i}, {j}, {k})'.format(i=self.cube_coords[0],
+                                                         j=self.cube_coords[1],
+                                                         k=self.cube_coords[2])
+        ax.set_title(title)
+        return ax
---
+++ tests.py
@@ -0,0 +1,133 @@
+import math, unittest
+
+from samples import BaseSample, Scan, Cube
+
+class SlamFileTest(unittest.TestCase):
+
+    def setUp(self):
+        self.sample = BaseSample(center=(0, 0), diameter=12.7, rows=2)
+        self.sample.two_theta_range = (50, 90)
+        self.frame_step = 15
+        self.frame_width = 20
+
+    def test_number_of_frames(self):
+        self.assertEqual(
+            self.sample.get_number_of_frames(),
+            3
+        )
+        # Check for values outside of limits
+        self.sample.two_theta_range = (50, 200)
+        self.assertRaises(
+            ValueError,
+            self.sample.get_number_of_frames
+        )
+
+    def test_theta2_start(self):
+        self.assertEqual(
+            self.sample.get_theta2_start(),
+            7.5
+        )
+
+    def test_theta1(self):
+        self.assertEqual(
+            self.sample.get_theta1(),
+            50
+        )
+        # Check for values outside the limits
+        self.sample.two_theta_range = (-5, 50)
+        self.assertRaises(
+            ValueError,
+            self.sample.get_theta1
+        )
+        # Check for values outside theta1 max
+        self.sample.two_theta_range = (60, 90)
+        self.assertEqual(
+            self.sample.get_theta1(),
+            50
+        )
+
+    def test_path(self):
+        results_list = []
+        for coords in self.sample.path(1):
+            results_list.append(coords)
+        self.assertEqual(
+            results_list,
+            [Cube(0, 0, 0),
+             Cube(1, 0, -1),
+             Cube(0, 1, -1),
+             Cube(-1, 1, 0),
+             Cube(-1, 0, 1),
+             Cube(0, -1, 1),
+             Cube(1, -1, 0)]
+        )
+        self.sample.create_scans()
+        self.assertEqual(
+            self.sample.scans[8].cube_coords,
+            Cube(2, 0, -2)
+        )
+
+    def test_cell_size(self):
+        sample = BaseSample(center=(0, 0), diameter=20, rows=5)
+        self.assertEqual(sample.unit_size, 4/math.sqrt(3))
+
+    def test_jinja_context(self):
+        sample = BaseSample(center=(0, 0), diameter=10, rows=4,
+                            sample_name='LiMn2O4')
+        sample.create_scans()
+        context = sample.get_context()
+        self.assertEqual(
+            len(context['scans']),
+            len(sample.scans)
+        )
+        self.assertEqual(
+            context['scans'][1]['x'],
+            sample.scans[1].xy_coords(sample.unit_size)[0]
+        )
+        self.assertEqual(
+            context['scans'][1]['y'],
+            sample.scans[1].xy_coords(sample.unit_size)[1]
+        )
+        self.assertEqual(
+            context['scans'][3]['filename'],
+            'LiMn2O4-00000003'
+        )
+
+class ScanTest(unittest.TestCase):
+    def setUp(self):
+        self.scan = Scan(Cube(1, 0, -1), 'sample')
+
+    def test_xy_coords(self):
+        self.scan.cube_coords = Cube(1, -1, 0)
+        self.assertEqual(
+            self.scan.xy_coords(1),
+            (1, 0)
+        )
+        self.scan.cube_coords = Cube(1, 0, -1)
+        self.assertEqual(
+            self.scan.xy_coords(1),
+            (0.5, math.sqrt(3)/2)
+        )
+        self.scan.cube_coords = Cube(0, 1, -1)
+        self.assertEqual(
+            self.scan.xy_coords(1),
+            (-0.5, math.sqrt(3)/2)
+        )
+        self.scan.cube_coords = Cube(1, -2, 1)
+        self.assertEqual(
+            self.scan.xy_coords(1),
+            (1.5, -math.sqrt(3)/2)
+        )
+        self.scan.cube_coords = Cube(2, 0, -2)
+        self.assertEqual(
+            self.scan.xy_coords(1),
+            (1, math.sqrt(3))
+        )
+
+    def test_unit_size(self):
+        self.assertEqual(
+            self.scan.xy_coords(2),
+            (1, math.sqrt(3))
+        )
+
+if __name__ == '__main__':
+    unittest.main()
